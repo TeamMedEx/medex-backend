13deef50ff9bd0d22357234dd653490a0bb693d0
ivanignatius <ivanignatius98@gmail.com>
2022-10-18 13:49:46 +0700

    added return token on register

--- app/controllers/v1/user.controller.js
+++ app/controllers/v1/user.controller.js
@@ -7,21 +7,43 @@ const { verifyToken } = require("../../utils/helper")
 
 const tokenExpiration = 60 * 60
 const refreshExpiration = 60 * 60 * 24
+
+const generateLoginTokens = (user) => {
+  const refreshToken = jwt.sign({}, process.env.SECRET_KEY, {
+    expiresIn: refreshExpiration
+  })
+  Object.assign(user, { refresh_token: refreshToken })
+  user.save()
+
+  const { username, email, name } = user.toJSON()
+  const token = jwt.sign({ username, email, name }, process.env.SECRET_KEY, {
+    expiresIn: tokenExpiration
+  })
+  const expiresAt = moment().add(tokenExpiration, 'seconds')
+  return { refreshToken, token, expiresAt }
+}
+
 module.exports = {
   store: async (req, res) => {
     console.log(`┌─ ${LOG} : save user`);
     const payload = req.body
-    const user = await User.findOne({ email: payload.email, username: payload.username }, '_id source').lean()
-
+    const { username, email } = payload
+    const user = await User.findOne({ $or: [{ username: username }, { email: email }] }, '_id source').lean()
+    console.log({ user })
     if (user != null) {
-      return res.locals.helpers.jsonFormat(400, message, { code: `registered_by_${user.source == undefined ? "email" : user.source.toLowerCase()}` })
+      return res.locals.helpers.jsonFormat(400, "User exists", { code: `registered_by_${user.source == undefined ? "email" : user.source.toLowerCase()}` })
     }
-
     Object.assign(payload, { updated_at: moment(), created_at: moment(), })
-
     try {
-      await User.create(payload)
-      return res.locals.helpers.jsonFormat(200, 'Success to save new user')
+      let tokenPayload = {}
+      const newUser = await User.create(payload)
+      const { refresh_token, expires_at } = payload
+      if (refresh_token != undefined && expires_at != undefined && payload.source.toLowerCase() == "google") {
+        tokenPayload = { refreshToken: payload.refresh_token, expiresAt: payload.expires_at, }
+      } else {
+        tokenPayload = generateLoginTokens(newUser)
+      }
+      return res.locals.helpers.jsonFormat(200, 'Success to save new user', tokenPayload)
     } catch ({ message }) {
       return res.locals.helpers.jsonFormat(400, message, null)
     }
@@ -96,18 +118,8 @@ module.exports = {
     if (!valid) {
       return res.locals.helpers.jsonFormat(400, 'Invalid password')
     }
-    const refreshToken = jwt.sign({}, process.env.SECRET_KEY, {
-      expiresIn: refreshExpiration
-    })
-    Object.assign(user, { refresh_token: refreshToken })
-    user.save()
-    const record = user.toJSON()
-    delete record.password
-    const token = jwt.sign(record, process.env.SECRET_KEY, {
-      expiresIn: tokenExpiration
-    })
-    const expiresAt = moment().add(tokenExpiration, 'seconds')
-    return res.locals.helpers.jsonFormat(200, 'Success login', { token, refreshToken, expiresAt })
+    const tokenPayload = generateLoginTokens(user)
+    return res.locals.helpers.jsonFormat(200, 'Success login', tokenPayload)
   },
   refreshToken: async (req, res) => {
     console.log(`┌─ ${LOG} : refresh token`)
